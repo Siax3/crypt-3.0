f22ff8f920001e9c612522540939eeed8f70f622
Siax3 <131237799+Siax3@users.noreply.github.com>
2023-12-20 15:45:56 +0100

    Add files via upload

---
+++ ClassTester/ClassTester.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "clsCrypto.h"
+#include "clsCryptoEx.h"
+
+int main()
+{
+    clsCryptoEx* p_lobjCryptoEx;
+
+    // vytvořit objekt
+    p_lobjCryptoEx = new clsCryptoEx();
+
+    // otevřít soubor
+    p_lobjCryptoEx->OpenFile(L"c:\\temp\\test.txt", L"c:\\temp\\testcrypt.txt");
+
+    // zašifrovat
+    p_lobjCryptoEx->Crypt();
+
+    // zavřít soubor
+    p_lobjCryptoEx->CloseFile();
+
+    // otevřít soubor
+    p_lobjCryptoEx->OpenFile(L"c:\\temp\\testcrypt.txt", L"c:\\temp\\testdecrypt.txt");
+
+    // odšifrovat
+    p_lobjCryptoEx->Decrypt();
+
+    // zavřít soubor
+    p_lobjCryptoEx->CloseFile();
+
+    // zrušit objekt
+    delete(p_lobjCryptoEx);
+}
